b2ca4126dbf59f76397926b5307c248437877581
Jesper Fogh <foghjesperjhf@gmail.com>
2019-02-07 14:54:42 +0100

    Added different css files

--- color.js
+++ color.js
@@ -1,7 +1,13 @@
+var mic;
+
 function setup() {
     createCanvas(400, 400);
+    mic = new p5.AudioIn()
+    mic.start();
 }
 
 function draw() {
-
+    background(0);
+    micLevel = mic.getLevel();
+    ellipse(width / 2, constrain(height - micLevel * height * 5, 0, height), 10, 10);
 }
