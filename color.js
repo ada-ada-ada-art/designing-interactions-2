8ea61ded39946155635ac99fa71603e7c68885c2
Jesper Fogh <foghjesperjhf@gmail.com>
2019-02-07 15:31:18 +0100

    Finished the basic version of the clap-to-color sketch

--- color.js
+++ color.js
@@ -1,13 +1,27 @@
-var mic;
+var mic, body, btn;
+var bodyColor = 0;
 
 function setup() {
     createCanvas(400, 400);
     mic = new p5.AudioIn()
     mic.start();
+    body = select('body');
+    btn = select('button');
 }
 
 function draw() {
     micLevel = mic.getLevel();
-    print(micLevel);
-    ellipse(width / 2, height / 2, micLevel, micLevel);
+    if(micLevel > 0.1) {
+    	console.log(micLevel + " / " + bodyColor);
+    	if(bodyColor < 80) {
+    		console.log(bodyColor);
+    		bodyColor++;
+    	}
+    }
+
+    btn.mouseClicked(function() {
+    	bodyColor = 0;
+    });
+
+    body.style('background', 'hsl(25, 47%, ' + bodyColor + '%)');
 }
